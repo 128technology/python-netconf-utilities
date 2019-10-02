e862e0490b9d7f0ed31e6ea6c6c8440b840d4974
Grant McCloskey <gmccloskey@128technology.com>
2019-10-02 12:02:33 -0400

    This addition will allow for running of a setup package command that will locally build all deps in requirements.txt and place them as .whl files in the "wheelhosue" dir. The whole package can then be tar-ed and installed in an totally local way (See ReadMe)

---
+++ package.py
@@ -0,0 +1,112 @@
+from setuptools import Command
+
+import shlex
+import subprocess
+import os
+
+WHEELHOUSE = "wheelhouse"
+
+
+class Package(Command):
+    """Package Code and Dependencies into wheelhouse"""
+
+    description = "Run wheels for dependencies and submodules dependencies"
+    user_options = []
+
+    def __init__(self, dist):
+        Command.__init__(self, dist)
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def localize_requirements(self):
+        """
+        After the package is unpacked at the target destination, the requirements can be installed
+        locally from the wheelhouse folder using the option --no-index on pip install which
+        ignores package index (only looking at --find-links URLs instead).
+
+        `pip install -r requirements.txt  --no-index --find-links wheelhouse`
+        """
+        dependencies = open("requirements.txt").read().split("\n")
+        local_dependencies = []
+
+        for dependency in dependencies:
+            if dependency:
+                if "egg=" in dependency:
+                    pkg_name = dependency.split("egg=")[-1]
+                    local_dependencies.append(pkg_name)
+                elif "git+" in dependency:
+                    pkg_name = dependency.split("/")[-1].split(".")[0]
+                    local_dependencies.append(pkg_name)
+                else:
+                    local_dependencies.append(dependency)
+
+        print(f"local packages in wheel: {local_dependencies}")
+        self.execute("mv requirements.txt requirements.orig")
+
+        with open("requirements.txt", "w") as requirements_file:
+            # filter is used to remove empty list members (None).
+            requirements_file.write("\n".join(filter(None, local_dependencies)))
+
+    def execute(self, command, capture_output=False):
+        """
+        The execute command will loop and keep on reading the stdout and check for the return code
+        and displays the output in real time.
+        """
+
+        print(f"Running shell command: {command}")
+
+        if capture_output:
+            return subprocess.check_output(shlex.split(command))
+
+        process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+
+        while True:
+            output = process.stdout.readline()
+
+            if output == b"" and process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+
+        return_code = process.poll()
+
+        if return_code != 0:
+            print(f"Error running command {command} - exit code: {return_code}")
+            raise IOError("Shell Commmand Failed")
+
+        return return_code
+
+    def run_commands(self, commands):
+        for command in commands:
+            self.execute(command)
+
+    def restore_requirements_txt(self):
+        if os.path.exists("requirements.orig"):
+            print("Restoring original requirements.txt file")
+            commands = ["rm requirements.txt", "mv requirements.orig requirements.txt"]
+            self.run_commands(commands)
+
+    def run(self):
+        commands = []
+        commands.extend(
+            [
+                "rm -rf {dir}".format(dir=WHEELHOUSE),
+                "mkdir -p {dir}".format(dir=WHEELHOUSE),
+                "pip3.6 wheel --wheel-dir={dir} -r requirements.txt".format(dir=WHEELHOUSE),
+            ]
+        )
+
+        print("Packing requirements.txt into wheelhouse")
+        self.run_commands(commands)
+        print("Generating local requirements.txt")
+        self.localize_requirements()
+
+        print("Packing code and wheelhouse into dist")
+        self.run_command("sdist")
+        self.restore_requirements_txt()
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+from package import Package
 
 setup(
-    name='ote_utils',
-    version='1.1.2',
-    description='collection of ote utils',
-    packages=['ote_utils' , 'ote_utils.linux',
-              'ote_utils.netconfutils', 'ote_utils.utils']
+    name="ote_utils",
+    version="1.1.2",
+    description="collection of ote utils",
+    packages=["ote_utils", "ote_utils.linux", "ote_utils.netconfutils", "ote_utils.utils"],
+    cmdclass={"package": Package},
 )
