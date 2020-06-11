98aebc4fc5e95fdc83e26d00caf80d836472d0cb
Tyler Carroll <tcarroll@128technology.com>
2020-06-11 14:53:16 -0400

    Add install requires for future package
     - Needed for python2/3 compatibility

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='ote_utils',
-    version='1.1.2',
-    description='collection of ote utils',
-    packages=['ote_utils' , 'ote_utils.linux',
-              'ote_utils.netconfutils', 'ote_utils.utils']
+    name="ote_utils",
+    version="1.1.2",
+    description="collection of ote utils",
+    packages=[
+        "ote_utils",
+        "ote_utils.linux",
+        "ote_utils.netconfutils",
+        "ote_utils.utils",
+    ],
+    install_requires=["future==0.16.0"],
 )
